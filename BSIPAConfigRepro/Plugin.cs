421f192e26f89882155e32543f893e71294727f5
Eris <erisapps@outlook.be>
2021-12-14 12:02:54 +0100

    🎉 The actual initial commit

---
+++ BSIPAConfigRepro/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Config.Stores;
+using IPA.Logging;
+
+namespace BSIPAConfigRepro
+{
+    [Plugin(RuntimeOptions.DynamicInit), NoEnableDisable]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(Logger logger, Config conf)
+        {
+            var config = conf.Generated<PluginConfig>();
+
+            logger.Notice($"A normal bool: {config.ANormalBool}");
+            logger.Notice($"A nullable bool: {config.ANullableBool}");
+        }
+    }
+}
---
+++ BSIPAConfigRepro/PluginConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+namespace BSIPAConfigRepro
+{
+    internal class PluginConfig
+    {
+        public virtual bool ANormalBool { get; set; }
+        public virtual bool? ANullableBool { get; set; }
+    }
+}
